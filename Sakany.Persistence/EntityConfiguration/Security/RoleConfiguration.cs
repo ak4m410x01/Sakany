d5b31c2bca0e2c5b8dbe53ff7a3e92e7a6ef0eeb
Abdullah Kamal <abdallah10kamal@gmail.com>
2024-06-24 01:26:09 +0300

    Add Identity Entity Type Configurations.

---
+++ Sakany.Persistence/EntityConfiguration/Security/RoleConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Sakany.Persistence.EntityConfiguration.Security
+{
+    public class RoleConfiguration : IEntityTypeConfiguration<IdentityRole>
+    {
+        public void Configure(EntityTypeBuilder<IdentityRole> builder)
+        {
+            #region Config Table Name
+
+            builder.ToTable("Roles", "Security");
+
+            #endregion Config Table Name
+        }
+    }
+}
---
+++ Sakany.Persistence/EntityConfiguration/Users/ApplicationUserConfiguration.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using Sakany.Domain.IdentityEntities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Sakany.Domain.Enumerations.Users;
+
+namespace Sakany.Persistence.EntityConfiguration.Users
+{
+    public class ApplicationUserConfiguration : IEntityTypeConfiguration<ApplicationUser>
+    {
+        public void Configure(EntityTypeBuilder<ApplicationUser> builder)
+        {
+            #region Config Table Name
+
+            builder.ToTable("Users", "User");
+
+            #endregion Config Table Name
+
+            #region Config Properties
+
+            builder.Property(user => user.Status)
+                   .HasDefaultValue(UserStatus.Active.ToString())
+                   .IsRequired();
+
+            builder.Property(user => user.FirstName)
+                   .HasMaxLength(100)
+                   .IsRequired(false);
+
+            builder.Property(user => user.LastName)
+                   .HasMaxLength(100)
+                   .IsRequired(false);
+
+            builder.Property(user => user.CreatedAt)
+                   .HasDefaultValue(DateTime.UtcNow)
+                   .IsRequired();
+
+            builder.Property(user => user.ModifiedAt)
+                   .HasDefaultValue(DateTime.UtcNow)
+                   .IsRequired();
+
+            builder.Property(user => user.DeletedAt)
+                   .IsRequired(false);
+
+            #endregion Config Properties
+        }
+    }
+}

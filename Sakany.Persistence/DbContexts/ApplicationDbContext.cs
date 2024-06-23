0ab3731a0453429dda25f92f23073ca00ac7ce91
Abdullah Kamal <abdallah10kamal@gmail.com>
2024-06-24 01:26:25 +0300

    Add DbContext.

---
+++ Sakany.Persistence/DbContexts/ApplicationDbContext.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Sakany.Domain.IdentityEntities;
+using System.Reflection;
+
+namespace Sakany.Persistence.DbContexts
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        #region Constructors
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        #endregion Constructors
+
+        #region Methods
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            #region Identity Configuration
+
+            // Config Identity Tables Names
+            builder.Entity<IdentityUserRole<string>>().ToTable("UserRoles", "Security");
+            builder.Entity<IdentityUserClaim<string>>().ToTable("UserClaims", "Security");
+            builder.Entity<IdentityUserLogin<string>>().ToTable("UserLogins", "Security");
+            builder.Entity<IdentityRoleClaim<string>>().ToTable("RoleClaims", "Security");
+            builder.Entity<IdentityUserToken<string>>().ToTable("UserTokens", "Security");
+
+            #endregion Identity Configuration
+
+            #region Assemblies Configuration
+
+            // Apply Entities Configurations
+            builder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+            #endregion Assemblies Configuration
+        }
+
+        #endregion Methods
+    }
+}

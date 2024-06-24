3539a70135be96df14c4ce1aab3631c2ae9ae123
Abdullah Kamal <abdallah10kamal@gmail.com>
2024-06-24 12:47:31 +0300

    Add Infrastructure Service Collection.

---
+++ Sakany.Infrastructure/Extensions/InfrastructureServiceCollection.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Sakany.Infrastructure.Extensions
+{
+    public static class InfrastructureServiceCollection
+    {
+        public static IServiceCollection AddInfrastructureLayer(this IServiceCollection services)
+        {
+            return services;
+        }
+    }
+}

17ebbf351c8ed149ac8c1668e316389e4ed2d65e
Abdullah Kamal <abdallah10kamal@gmail.com>
2024-06-24 12:58:44 +0300

    Add AutoMapper Configuration.

--- Sakany.Application/Extensions/ApplicationServiceCollection.cs
+++ Sakany.Application/Extensions/ApplicationServiceCollection.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
+using Sakany.Application.Extensions.AutoMapper;
 
 namespace Sakany.Application.Extensions
 {
@@ -6,6 +7,12 @@ namespace Sakany.Application.Extensions
     {
         public static IServiceCollection AddApplicationLayer(this IServiceCollection services)
         {
+            #region Configure AutoMapper
+
+            services.AddAutoMapperConfiguration();
+
+            #endregion Configure AutoMapper
+
             return services;
         }
     }
---
+++ Sakany.Application/Extensions/AutoMapper/AutoMapperServiceCollection.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace Sakany.Application.Extensions.AutoMapper
+{
+    internal static class AutoMapperServiceCollection
+    {
+        public static IServiceCollection AddAutoMapperConfiguration(this IServiceCollection services)
+        {
+            services.AddAutoMapper(Assembly.GetExecutingAssembly());
+            return services;
+        }
+    }
+}

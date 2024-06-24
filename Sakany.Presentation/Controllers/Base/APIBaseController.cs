8bf68560005736bb548f0193a253977ea3f13dc8
Abdullah Kamal <abdallah10kamal@gmail.com>
2024-06-24 13:53:17 +0300

    Add API Base Controller.

---
+++ Sakany.Presentation/Controllers/Base/APIBaseController.cs
@@ -0,0 +1,45 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Sakany.Shared.Responses;
+using System.Net;
+
+namespace Sakany.Presentation.Controllers.Base
+{
+    [Route("Api/V{version:apiVersion}/[controller]")]
+    [ApiController]
+    public class APIBaseController : ControllerBase
+    {
+        #region Properties
+
+        public IMediator Mediator { get; protected set; }
+
+        #endregion Properties
+
+        #region Constructors
+
+        public APIBaseController(IMediator mediator)
+        {
+            Mediator = mediator;
+        }
+
+        #endregion Constructors
+
+        #region Methods
+
+        public IActionResult ResponseResult<T>(Response<T> response)
+        {
+            return response.StatusCode switch
+            {
+                HttpStatusCode.OK => Ok(response),
+                HttpStatusCode.Created => Created("", response),
+                HttpStatusCode.Unauthorized => Unauthorized(response),
+                HttpStatusCode.BadRequest => BadRequest(response),
+                HttpStatusCode.NotFound => NotFound(response),
+                HttpStatusCode.NoContent => Ok(response),
+                _ => Ok(response)
+            };
+        }
+
+        #endregion Methods
+    }
+}

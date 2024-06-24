3593d9366971557b43c85a9d3f7a371111388f0f
Abdullah Kamal <abdallah10kamal@gmail.com>
2024-06-24 12:37:12 +0300

    Add JwtSettings Helper.

---
+++ Sakany.Shared/Helpers/JwtSettings/JwtSettings.cs
@@ -0,0 +1,11 @@
+﻿namespace Sakany.Shared.Helpers.JwtSettings
+{
+    public class JwtSettings
+    {
+        public string? Issuer { get; set; }
+        public string? Audience { get; set; }
+        public int AccessTokenExpiryDays { get; set; }
+        public int RefreshTokenExpiryDays { get; set; }
+        public string? Key { get; set; }
+    }
+}
